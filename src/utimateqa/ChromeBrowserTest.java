94219568664deb9105c9f9b82c70191b2ad10721
rpatel942017 <rpatel942017@gmail.com>
2023-04-09 22:50:13 +0100

    update

--- src/utimateqa/ChromeBrowserTest.java
+++ src/utimateqa/ChromeBrowserTest.java
@@ -1,2 +1,50 @@
-package utimateqa;public class ChromeBrowserTest {
+package utimateqa;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class ChromeBrowserTest {
+
+    public static void main(String[] args) {
+
+
+        String baseUrl = "https://courses.ultimateqa.com/users/sign_in";
+
+        //Launch the Chrome Browser
+        WebDriver driver = new ChromeDriver();
+
+        // Open the URL into Browser
+        driver.get(baseUrl);
+        //maximise the Browser
+        driver.manage().window().maximize();
+        //We give Implicit wait to driver
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+
+        //Get the title of the page
+        String title = driver.getTitle();
+        System.out.println(title);
+
+        //Get the current URL
+        System.out.println("Current URL : " + driver.getCurrentUrl());
+
+        //Get page source
+        System.out.println("Page source : " + driver.getPageSource());
+
+        // Find the email field element
+        WebElement emailField = driver.findElement(By.id("user[email]"));
+        emailField.sendKeys("prime123@gmail.com");
+
+
+        // Find the password field element and type the password
+        driver.findElement(By.name("user[password]")).sendKeys("prime123");
+
+        //Close the browser
+        driver.close();
+
+    }
+
 }
--- src/utimateqa/MultiBrowserTest.java
+++ src/utimateqa/MultiBrowserTest.java
@@ -1,2 +1,55 @@
-package utimateqa;public class MultiBrowserTest {
+package utimateqa;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+
+public class MultiBrowserTest {
+
+    static String browser = "Chrome";
+    static String baseUrl = "https://courses.ultimateqa.com/users/sign_in";
+    static WebDriver driver;
+
+    public static void main(String[] args) {
+        if (browser.equalsIgnoreCase("Chrome")) {
+            driver = new ChromeDriver();
+        } else if (browser.equalsIgnoreCase("FireFox")) {
+            driver = new FirefoxDriver();
+        } else if (browser.equalsIgnoreCase("Edge")) {
+            driver = new EdgeDriver();
+        } else {
+            System.out.println("Wrong Browser name");
+        }
+
+        // Open the URL into Browser
+        driver.get(baseUrl);
+        //maximise the Browser
+        driver.manage().window().maximize();
+        //We give Implicit wait to driver
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+
+        //Get the title of the page
+        String title = driver.getTitle();
+        System.out.println(title);
+
+        //Get the current URL
+        System.out.println("Current URL : " + driver.getCurrentUrl());
+
+        //Get page source
+        System.out.println("Page source : " + driver.getPageSource());
+
+        // Find the email field element
+        WebElement emailField = driver.findElement(By.id("user[email]"));
+        emailField.sendKeys("prime123@gmail.com");
+
+
+        // Find the password field element and type the password
+        driver.findElement(By.name("user[password]")).sendKeys("prime123");
+
+    }
 }
